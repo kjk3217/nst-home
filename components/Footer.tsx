5ca1722ba5f55f48a0a707275b5d4d6ce7d98c53
kjk3217 <kjk3217@naver.com>
2025-11-28 10:09:24 +0900

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-[#0b1c2d] text-gray-400 py-16">
       <div className="container mx-auto px-4 md:px-6">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
           
           {/* Brand Info */}
           <div className="col-span-1 md:col-span-2">
@@ -22,17 +22,7 @@ const Footer: React.FC = () => {
             </div>
           </div>
 
-          {/* Quick Links */}
-          <div>
-            <h4 className="text-white font-bold text-lg mb-6">Quick Links</h4>
-            <ul className="space-y-3">
-              <li><a href="#" className="hover:text-nst-teal transition-colors">Company Intro</a></li>
-              <li><a href="#" className="hover:text-nst-teal transition-colors">NST Method</a></li>
-              <li><a href="#" className="hover:text-nst-teal transition-colors">Portfolio</a></li>
-              <li><a href="#" className="hover:text-nst-teal transition-colors">Customer Reviews</a></li>
-              <li><a href="#" className="hover:text-nst-teal transition-colors">Contact Us</a></li>
-            </ul>
-          </div>
+          {/* Quick Links 섹션은 삭제되었습니다. */}
 
           {/* Contact Info */}
           <div>
